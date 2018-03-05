0e38f78bc69aa25c6ab7ae04e62124f839dd8da7
Madushi <madushi.15@cse.mrt.ac.lk>
2018-03-05 14:29:04 +0530

    myapp

---
+++ src/pages/home/home.php
@@ -0,0 +1,27 @@
+<?php
+echo"gvyt"
+
+
+?>
+<ion-header style="background: orange">
+  <ion-navbar style="background: orange">
+    <ion-title>Sign In</ion-title>
+    <button ion-button color="dark" style="margin-left: 250px;font-weight: bold" clear>Next</button>
+    
+  </ion-navbar>
+</ion-header>
+
+<ion-content padding style=" background:url('../../assets/imgs/1.jpg') no-repeat center center fixed">
+  <h2 style="margin-bottom: 100px;margin-top: 20px;color: white">Welcome to Kitchen Hero!!</h2>
+ 
+  <ion-list>
+
+    <ion-item style="background-color: transparent">
+      <ion-label floating style="font-weight: bold" >Enter Pin Number</ion-label>
+      <ion-input type="text"></ion-input>
+    </ion-item>
+
+
+
+</ion-list>
+</ion-content>
